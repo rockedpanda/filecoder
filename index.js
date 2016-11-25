863e7bffa2a234d0f8d8f5664777431cc6d5d7de
rockedpanda <chenghuawei@gmail.com>
2016-11-25 09:52:48 +0800

    add fs link method

--- index.js
+++ index.js
@@ -4,6 +4,64 @@ const iconv = require('iconv-lite');
 var defaultInputCode = 'gbk';
 var defaultOutputCode = 'utf8';
 
+/** 实现对fs常用属性扩展的类 */
+function CoderFs(defaultCode){
+    this.defaultCode = defaultCode || 'utf8';
+}
+CoderFs.prototype.readFile = function(file,options,callback) {
+    if(arguments.length === 2){
+        callback = options;
+        options = this.defaultCode;
+    } else if(arguments.length === 3){
+        if(options instanceof Object && !(options instanceof String) && !('encoding' in options)){
+            options.encoding = this.defaultCode;
+        }
+    }
+    fs.readFile(file, options, callback);
+};
+CoderFs.prototype.readFileSync = function(file,options) {
+    if(options instanceof Object && !(options instanceof String) && !('encoding' in options)){
+        options.encoding = this.defaultCode;
+    }
+    return fs.readFileSync(file, options);
+};
+CoderFs.prototype.createReadStream = function(path, options, targetCode) {
+    targetCode = targetCode || 'utf8';
+    let code = this.defaultCode;
+    if(options === undefined){
+        code = this.defaultCode;
+        options = undefined;
+    } else if(typeof options ==='string' || options instanceof String){
+        code = options;
+        options = undefined;
+    }
+    else if('encoding' in options){
+        code = options.encoding;
+        options.encoding = null;
+    }
+    console.log(options);
+    return fs.createReadStream(path, options)
+        .pipe(iconv.decodeStream(code))
+        .pipe(iconv.encodeStream(targetCode));
+};
+CoderFs.prototype.createWriteStream = function(path, options, sourceCode) {
+    sourceCode = sourceCode || 'utf8';
+    let code = this.defaultCode;
+     if(options === undefined){
+        code = this.defaultCode;
+        options = undefined;
+    } else if(typeof options ==='string' || options instanceof String){
+        code = options;
+        options = undefined;
+    }
+    else if('encoding' in options){
+        code = options.encoding;
+        options.encoding = null;
+    }
+    let fsStream = fs.createWriteStream(path, options);
+    return iconv.decodeStream(sourceCode).pipe(iconv.encodeStream(code)).pipe(fsStream);
+};
+
 /**
  * 设置默认的编码格式
  * @param {Object} option {'org':'gbk','dest':'utf8'}
@@ -43,8 +101,13 @@ function changeStr(str, orgCode, destCode) {
     return iconv.encode("Sample input string", destCode);
 }
 
+function getFs(code){
+    return new CoderFs(code);
+}
+
 exports.getStr = getStr;
 exports.changeStr = changeStr;
 exports.changeFile = changeFile;
 exports.changeFileSync = changeFileSync;
 exports.setDefaultCoding = setDefaultCoding;
+exports.getFs = getFs;
--- test/basic_test.js
+++ test/basic_test.js
@@ -1,4 +1,11 @@
 'use strict';
+var fs = require('fs');
 var coder = require('../index.js');
 
 console.log(coder.changeStr('中文UTF8测试','utf8','gbk'));
+var cfs = coder.getFs('gbk');
+cfs.createReadStream('gbk.txt').pipe(process.stdout);
+cfs.createReadStream('gbk.txt').pipe(fs.createWriteStream('uu.txt','utf8'));
+
+fs.createReadStream('utf.txt','utf8').pipe(cfs.createWriteStream('gg.txt','gbk'));
+fs.createReadStream('utf.txt','utf8').pipe(process.stdout);
